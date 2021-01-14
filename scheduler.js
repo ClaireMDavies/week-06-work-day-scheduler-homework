bd81ab9067235be0c19ffbc2cde912a39f034d2e
Claire Davies <claire.davies@junitech.com>
2021-01-14 17:50:35 +0000

    dynamic creation of css of timeblocks

--- scheduler.js
+++ scheduler.js
@@ -2,14 +2,37 @@
     
 
 $(document).ready(function()
-{
+{   //retrieving the day
     var todayDate = moment().format("dddd, MMMM Do YYYY");
-    var currentTime = moment().format("HH");
-    var pastTime;
-    var futureTime;
+    //retrieving the hour
+    var currentHour = moment().format("HH");
 
+    //displaying current day
     $("#currentDay").append(todayDate);
-    console.log(currentTime);
-});
 
+    $(".description").each(function(index) {
+        
+        // get the id of each row description so we can extract the hour from it
+        var rowId = $(this).attr("id");
+
+        // the hour is the last two characters of the id
+        rowHour = parseInt(rowId.slice(-2));
+
+        if (currentHour > rowHour)
+        {
+            $(this).addClass("past");
+        }
+        else if (currentHour < rowHour)
+        {
+            $(this).addClass("future");
+        }
+        else
+        {
+            // current hour
+            $(this).addClass("present");
+        }
+    });
+
+    
+});
 
