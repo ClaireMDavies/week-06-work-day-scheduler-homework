d44ba8a051b0276ae708120f6647d272435e4310
Claire Davies <claire.davies@junitech.com>
2021-02-13 20:49:42 +0000

    formatted js file

--- scheduler.js
+++ scheduler.js
@@ -1,8 +1,7 @@
 
-    
 
-$(document).ready(function()
-{  
+
+$(document).ready(function () {
     //writing local storage data to page on page load
     loadSchedule();
 
@@ -14,45 +13,42 @@ $(document).ready(function()
     //displaying current day
     $("#currentDay").append(todayDate);
 
-    $(".description").each(function(index) {
-        
+    $(".description").each(function (index) {
+
         // get the id of each row description so we can extract the hour from it
         var rowId = $(this).attr("id");
 
         // the hour is the last two characters of the id
         rowHour = parseInt(rowId.slice(-2));
 
-        if (currentHour > rowHour)
-        {
+        if (currentHour > rowHour) {
             $(this).addClass("past");
         }
-        else if (currentHour < rowHour)
-        {
+        else if (currentHour < rowHour) {
             $(this).addClass("future");
         }
-        else
-        {
+        else {
             // current hour
             $(this).addClass("present");
         }
     });
 
     //saving the information in relevant hour to local storage
-    $(".saveBtn").click(function() {
+    $(".saveBtn").click(function () {
 
         var buttonId = $(this).attr("id");
         var textAreaId = buttonId.slice(-7);
-        
+
         var rowText = $("#" + textAreaId).val();
         localStorage.setItem(textAreaId, rowText);
-        
+
     });
 });
 
 //retrieving info from local storage
 function loadSchedule() {
 
-    $(".description").each(function(index) {
+    $(".description").each(function (index) {
         var rowId = $(this).attr("id");
         var hourText = localStorage.getItem(rowId);
 
